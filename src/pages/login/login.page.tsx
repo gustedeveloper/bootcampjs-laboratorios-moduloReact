d8f814add56195c0ea90993b9e905d69f741be41
Guste Gaubaite <219.guste@gmail.com>
2024-09-16 15:38:16 +0200

    Add basic links to check navigation between pages

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { appRoutes } from "@/core/router";
+import { Link } from "react-router-dom";
 
 export const LoginPage: React.FC = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      Login
+      <br />
+      <Link to={appRoutes.accountList}>Mis cuentas</Link>
+    </div>
+  );
 };

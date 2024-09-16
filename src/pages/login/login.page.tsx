cb842f6821afa8d4719d9a57c30823f7deab263f
Guste Gaubaite <219.guste@gmail.com>
2024-09-16 16:14:52 +0200

    Add functionality to login page

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,17 +1,44 @@
 import React from "react";
+import { createEmptyCredentials, Credentials } from "./login.vm";
 
 export const LoginPage: React.FC = () => {
+  const [credentials, setCredentials] = React.useState<Credentials>(
+    createEmptyCredentials()
+  );
+
+  const handleFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(credentials);
+  };
+
   return (
     <div>
       <h1>Acceso</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Usuario</label>
-          <input type="text" id="username"></input>
+          <input
+            type="text"
+            id="username"
+            name="user"
+            onChange={handleFieldChange}
+          ></input>
         </div>
         <div>
           <label htmlFor="password">Contraseña</label>
-          <input type="password" id="password"></input>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={handleFieldChange}
+          ></input>
         </div>
         <button type="submit">Acceder</button>
       </form>

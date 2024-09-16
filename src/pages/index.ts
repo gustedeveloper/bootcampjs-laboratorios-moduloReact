b106df3000a83268c510dee3dfe5abb1269828b1
Guste Gaubaite <219.guste@gmail.com>
2024-09-16 15:04:09 +0200

    Create pages barrel file

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export * from "./account";
+export * from "./account-list";
+export * from "./create-account";
+export * from "./login";
+export * from "./movement-list";
+export * from "./transfer";

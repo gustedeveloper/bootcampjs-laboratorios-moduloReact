979ea79e5cc5b3c03cc70ac3448e1bd20820709a
Guste Gaubaite <219.guste@gmail.com>
2024-09-17 09:20:42 +0200

    Add styled header component to layout

--- src/layouts/app/app-layout.tsx
+++ src/layouts/app/app-layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HeaderComponent } from "./components";
 
 interface Props {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ export const AppLayout: React.FC<Props> = (props) => {
 
   return (
     <>
+      <HeaderComponent />
       <h1>Aquí iría la cabecera</h1>
       {children}
       <h1>Aquí el footer</h1>

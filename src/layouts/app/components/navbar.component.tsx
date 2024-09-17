5a2df7f31f458e351b7781566ee1541c681b1d16
Guste Gaubaite <219.guste@gmail.com>
2024-09-17 11:37:09 +0200

    Set the selected style to appear only on the selected page

--- src/layouts/app/components/navbar.component.tsx
+++ src/layouts/app/components/navbar.component.tsx
@@ -1,16 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { appRoutes } from "@/core/router";
+import { Link, useLocation } from "react-router-dom";
+import { appRoutes, routesPrefixes } from "@/core/router";
 import classes from "./navbar.component.module.css";
 
 export const NavBarComponent: React.FC = () => {
+  const { pathname } = useLocation();
+
   return (
     <nav className={classes.navbar}>
       <ul className={classes.list}>
-        <li className={classes.selected}>
+        <li
+          className={
+            pathname.startsWith(routesPrefixes.accountList)
+              ? classes.selected
+              : ""
+          }
+        >
           <Link to={appRoutes.accountList}>Mis Cuentas</Link>
         </li>
-        <li>
+        <li
+          className={
+            pathname.startsWith(routesPrefixes.transfer) ? classes.selected : ""
+          }
+        >
           <Link to={appRoutes.transfer}>Transferencias</Link>
         </li>
       </ul>

577051228a3b8ad4c1c6932b442488f9eb4f10c7
Guste Gaubaite <219.guste@gmail.com>
2024-09-17 09:42:22 +0200

    Add layout to transfer page

--- src/pages/transfer/transfer.page.tsx
+++ src/pages/transfer/transfer.page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import { AppLayout } from "@/layouts";
 
 export const TransferPage: React.FC = () => {
-  return <div>Transfer</div>;
+  return <AppLayout>Transfer</AppLayout>;
 };

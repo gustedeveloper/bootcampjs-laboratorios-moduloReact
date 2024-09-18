ec5217fb56a2e109ecc4277ea7c4bf9dd34c657d
Guste Gaubaite <219.guste@gmail.com>
2024-09-18 11:45:49 +0200

    Add options to the selector and add a navigation path to each option

--- src/pages/account-list/components/account-list-item.component.tsx
+++ src/pages/account-list/components/account-list-item.component.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { AccountVM } from "../account-list.vm";
 import classes from "./account-list-item.component.module.css";
-import { generatePath, Link } from "react-router-dom";
+import { generatePath, Link, useNavigate } from "react-router-dom";
 import { appRoutes } from "@/core/router";
+
+const ACTION_NONE = "";
+const ACTION_TRANSFER = "1";
+const ACTION_MOVEMENTS = "2";
 
 interface Props {
   accountItem: AccountVM;
@@ -10,6 +14,22 @@ interface Props {
 
 export const AccountListItemComponent: React.FC<Props> = (props) => {
   const { accountItem } = props;
+  const navigate = useNavigate();
+
+  const handleSelectedOptionChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    switch (e.target.value) {
+      case ACTION_TRANSFER:
+        navigate(
+          generatePath(appRoutes.transferFromAccount, { id: accountItem.id })
+        );
+        break;
+      case ACTION_MOVEMENTS:
+        navigate(generatePath(appRoutes.movements, { id: accountItem.id }));
+        break;
+    }
+  };
 
   return (
     <div className={classes.row}>
@@ -26,7 +46,14 @@ export const AccountListItemComponent: React.FC<Props> = (props) => {
         {accountItem.lastTransaction.toLocaleDateString()}
       </span>
       <span className={`${classes.dataCell} ${classes.selectContainer}`}>
-        <select className={classes.select}></select>
+        <select
+          className={classes.select}
+          onChange={handleSelectedOptionChange}
+        >
+          <option value={ACTION_NONE}>Seleccionar</option>
+          <option value={ACTION_TRANSFER}>Transferir</option>
+          <option value={ACTION_MOVEMENTS}>Movimientos</option>
+        </select>
       </span>
     </div>
   );

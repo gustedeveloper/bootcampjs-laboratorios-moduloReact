824eabf41dbd18e3b72d4665a98868bf6d8a8c5a
Guste Gaubaite <219.guste@gmail.com>
2024-09-18 12:00:42 +0200

    Delete mock data and get account list data from the api and add it to the page

--- src/pages/account-list/account-list.page.tsx
+++ src/pages/account-list/account-list.page.tsx
@@ -2,35 +2,21 @@ import React from "react";
 import { AppLayout } from "@/layouts";
 import { AccountVM } from "./account-list.vm";
 import classes from "./account-list.page.module.css";
-import { AccountListTableComponent } from "./components";
-import { AccountListHeaderComponent } from "./components/account-list-header.componennt";
-
-const mockAccountListData: AccountVM[] = [
-  {
-    id: "1",
-    iban: "ES91 2100 0418 4502 0005 1332",
-    name: "Personal",
-    balance: "1490",
-    lastTransaction: new Date("2019-12-09T21:30:00"),
-  },
-  {
-    id: "2",
-    iban: "ES79 2100 0813 6101 2345 6789",
-    name: "Compartida",
-    balance: "2480",
-    lastTransaction: new Date("2019-11-21T14:07:38"),
-  },
-  {
-    id: "3",
-    iban: "ES66 2100 0418 4012 3456 7891",
-    name: "Ahorro",
-    balance: "8500",
-    lastTransaction: new Date("2019-11-15T08:29:04"),
-  },
-];
+import {
+  AccountListTableComponent,
+  AccountListHeaderComponent,
+} from "./components";
+import { getAccountList } from "./api";
+import { mapAccountListFromApiToVm } from "./account-list.mapper";
 
 export const AccountListPage: React.FC = () => {
-  const [accountList] = React.useState<AccountVM[]>(mockAccountListData);
+  const [accountList, setAccountList] = React.useState<AccountVM[]>([]);
+
+  React.useEffect(() => {
+    getAccountList().then((result) =>
+      setAccountList(mapAccountListFromApiToVm(result))
+    );
+  }, []);
 
   return (
     <AppLayout>

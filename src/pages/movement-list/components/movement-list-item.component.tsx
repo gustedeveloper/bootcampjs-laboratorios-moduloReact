60131f5e12cf687e6b484042c8ed7ba12b60f511
Guste Gaubaite <219.guste@gmail.com>
2024-09-19 16:37:09 +0200

    Create and style each item of the movement list

---
+++ src/pages/movement-list/components/movement-list-item.component.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { MovementVM } from "../movement-list.vm";
+import classes from "./movement-list-item.component.module.css";
+
+interface Props {
+  movementItem: MovementVM;
+}
+
+export const MovementListItemComponent: React.FC<Props> = (props) => {
+  const { movementItem } = props;
+
+  return (
+    <div className={classes.row}>
+      <span className={classes.dataCell}>
+        {movementItem.transaction.toLocaleDateString()}
+      </span>
+      <span className={classes.dataCell}>
+        {movementItem.realTransaction.toLocaleDateString()}
+      </span>
+      <span className={classes.dataCell}>{movementItem.description}</span>
+      <span
+        className={`${classes.dataCell} ${classes.alignRight} ${
+          movementItem.amount.startsWith("-") ? classes.charge : ""
+        }`}
+      >
+        {movementItem.amount}€
+      </span>
+      <span className={`${classes.dataCell} ${classes.alignRight}`}>
+        {movementItem.balance}€
+      </span>
+    </div>
+  );
+};

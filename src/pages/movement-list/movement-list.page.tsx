2d9af78d8efbc4f611cc25521dfcff70b1820244
Guste Gaubaite <219.guste@gmail.com>
2024-09-19 18:21:50 +0200

    Delete mock data and get movements data from api

--- src/pages/movement-list/movement-list.page.tsx
+++ src/pages/movement-list/movement-list.page.tsx
@@ -9,49 +9,26 @@ import {
 import classes from "./movement-list.page.module.css";
 import { Account, createEmptyAccount, getAccount } from "./api";
 import { MovementListTableComponent } from "./components/movement-list-table.component";
-
-const mockMovementListData: MovementVM[] = [
-  {
-    id: "1",
-    description: "Nómina noviembre",
-    amount: "900",
-    balance: "1490",
-    transaction: new Date("2019-12-09T21:30:00"),
-    realTransaction: new Date("2019-12-09T21:30:00"),
-    accountId: "1",
-  },
-  {
-    id: "2",
-    description: "Alquiler noviembre",
-    amount: "-400",
-    balance: "590",
-    transaction: new Date("2019-12-07T11:30:00"),
-    realTransaction: new Date("2019-12-08T20:00:10"),
-    accountId: "1",
-  },
-  {
-    id: "3",
-    description: "Gastos móvil",
-    amount: "-24",
-    balance: "990",
-    transaction: new Date("2019-12-01T07:01:00"),
-    realTransaction: new Date("2019-12-02T12:00:10"),
-    accountId: "1",
-  },
-];
+import { getMovements } from "./api/movement-list.api";
+import { mapMovementListFromApiToVm } from "./movement-list.mapper";
 
 export const MovementListPage: React.FC = () => {
   const location = useLocation();
   const pathname = location.pathname;
   const accountId = pathname[pathname.length - 1];
 
-  const [movementList /*setMovementList*/] =
-    React.useState<MovementVM[]>(mockMovementListData);
+  const [movementList, setMovementList] = React.useState<MovementVM[]>([]);
 
   const [account, setAccount] = React.useState<Account>(createEmptyAccount());
 
   React.useEffect(() => {
-    getAccount(accountId).then((data) => setAccount(data));
+    getAccount(accountId).then((result) => setAccount(result));
+  }, []);
+
+  React.useEffect(() => {
+    getMovements(accountId).then((result) =>
+      setMovementList(mapMovementListFromApiToVm(result))
+    );
   }, []);
 
   return (

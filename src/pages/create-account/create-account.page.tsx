16de41f8b349e6be40c02becddd2cef932533db9
Guste Gaubaite <219.guste@gmail.com>
2024-09-23 23:24:57 +0200

    Create and style create account page header

--- src/pages/create-account/create-account.page.tsx
+++ src/pages/create-account/create-account.page.tsx
@@ -1,10 +1,13 @@
 import { AppLayout } from "@/layouts";
 import React from "react";
+import classes from "./create-account.page.module.css";
 
 export const CreateAccount: React.FC = () => {
   return (
     <AppLayout>
-      <div>Create a new account</div>
+      <div className={classes.container}>
+        <h1 className={classes.title}>Cuenta Bancaria</h1>
+      </div>
     </AppLayout>
   );
 };

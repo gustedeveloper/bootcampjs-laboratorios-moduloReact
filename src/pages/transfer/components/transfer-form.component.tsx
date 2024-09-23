a9025197f73842eb048bdca98c7b97e0005a3ed7
Guste Gaubaite <219.guste@gmail.com>
2024-09-23 11:17:29 +0200

    Define the values and handle the field change in each of the form's inputs

--- src/pages/transfer/components/transfer-form.component.tsx
+++ src/pages/transfer/components/transfer-form.component.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const TransferFormComponent: React.FC<Props> = (props) => {
   const { accountList, onTransfer } = props;
-  const [transfer /*setTransfer*/] = React.useState<TransferVM>(
+  const [transfer, setTransfer] = React.useState<TransferVM>(
     createEmptyTransferVm()
   );
 
@@ -17,39 +17,52 @@ export const TransferFormComponent: React.FC<Props> = (props) => {
     onTransfer(transfer);
   };
 
+  const handleFieldChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setTransfer({ ...transfer, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
       <h2>Transfer</h2>
       <form onSubmit={handleSubmit}>
         <div>
           <label>Seleccione cuenta origen</label>
-          <select>
+          <select
+            name="accountId"
+            onChange={handleFieldChange}
+            value={transfer.accountId}
+          >
             {accountList.map((account) => (
               <option key={account.id} value={account.id}>
                 {account.alias}
               </option>
             ))}
+            <option value="">Seleccione una cuenta</option>
           </select>
         </div>
         <div>
           <label>Ingrese el IBAN de destino:</label>
-          <input />
+          <input name="iban" onChange={handleFieldChange} />
         </div>
         <div>
           <label>Beneficiario:</label>
-          <input />
+          <input name="name" onChange={handleFieldChange} />
         </div>
         <div>
           <label>Importe:</label>
-          <input type="number" />
+          <input type="number" name="amount" onChange={handleFieldChange} />
         </div>
         <div>
           <label>Concepto:</label>
-          <input name="concept" />
+          <input name="concept" onChange={handleFieldChange} />
         </div>
         <div>
           <label>Observaciones:</label>
-          <input name="notes" />
+          <input name="notes" onChange={handleFieldChange} />
         </div>
         <div>
           <p>
@@ -58,14 +71,18 @@ export const TransferFormComponent: React.FC<Props> = (props) => {
           </p>
           <div>
             <label>Fecha de ejecución:</label>
-            <input name="realDateTransfer" type="date" />
+            <input
+              name="realDateTransfer"
+              type="date"
+              onChange={handleFieldChange}
+            />
           </div>
         </div>
         <div>
           <p>Escriba una dirección de email para dar aviso al beneficiario:</p>
           <div>
             <label>Email del beneficiario:</label>
-            <input name="email" />
+            <input name="email" onChange={handleFieldChange} />
           </div>
         </div>
         <button type="submit">REALIZAR LA TRANSFERENCIA</button>

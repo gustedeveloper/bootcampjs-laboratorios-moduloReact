b54b995b226cb4141ace3b7b0fbc61e4d9d1b66c
Guste Gaubaite <219.guste@gmail.com>
2024-09-23 23:17:30 +0200

    Add link to create account page to button of account list header component

--- src/pages/account-list/components/account-list-header.component.tsx
+++ src/pages/account-list/components/account-list-header.component.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import classes from "../account-list.page.module.css";
+import { Link } from "react-router-dom";
+import { appRoutes } from "@/core/router";
 
 export const AccountListHeaderComponent: React.FC = () => {
   return (
     <div className={classes.headerContainer}>
       <h1>Mis cuentas</h1>
-      <button>AGREGAR NUEVA CUENTA</button>
+      <button>
+        <Link to={appRoutes.createAccount}>AGREGAR NUEVA CUENTA</Link>
+      </button>
     </div>
   );
 };

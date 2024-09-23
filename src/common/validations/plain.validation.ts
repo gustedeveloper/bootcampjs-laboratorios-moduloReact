88b9f43765e77baf379ddbebaa4685ce1bd7d7ad
Guste Gaubaite <219.guste@gmail.com>
2024-09-23 12:52:40 +0200

    Add value not null or undefined validation

--- src/common/validations/plain.validation.ts
+++ src/common/validations/plain.validation.ts
@@ -9,3 +9,7 @@ export const isDateAfterToday = (date: Date): boolean => date > new Date();
 export const isEmailWellFormed = (email: string) => validator.isEmail(email);
 
 export const isStringValueInformed = (field: string) => field !== "";
+
+export const isValueNotNullOrUndefined = <T>(value: T): boolean => {
+  return value !== null && value !== undefined;
+};

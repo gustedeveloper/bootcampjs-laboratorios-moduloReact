ae4bd6088d692a7e1e6046efb114670c551d7479
Guste Gaubaite <219.guste@gmail.com>
2024-09-24 10:23:37 +0200

    Create account mock data and add it to the selection options

--- src/pages/create-account/components/create-account.form.component.tsx
+++ src/pages/create-account/components/create-account.form.component.tsx
@@ -1,20 +1,51 @@
 import React from "react";
+import { Account, accountMock } from "../create-account.vm";
 import classes from "./create-account.form.component.module.css";
 
 export const CreateAccountFormComponent: React.FC = () => {
+  const [account, setAccount] = React.useState<Account[]>([]);
+  React.useEffect(() => {
+    setAccount(accountMock);
+  }, []);
+
+  const handleFieldChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setAccount({ ...account, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className={classes.formContainer}>
           <div>
             <label>Tipo de cuenta:</label>
-            <select name="accountId" className={classes.medium}>
+            <select
+              name="type"
+              onChange={handleFieldChange}
+              className={classes.medium}
+            >
               <option value="">Seleccionar</option>
+              {account.map((a) => (
+                <option key={a.type} value={a.type}>
+                  {a.name}
+                </option>
+              ))}
             </select>
           </div>
           <div>
             <label>Alias:</label>
-            <input name="name" className={classes.medium} />
+            <input
+              name="name"
+              onChange={handleFieldChange}
+              className={classes.medium}
+            />
           </div>
         </div>
         <div>
--- src/pages/create-account/create-account.vm.ts
+++ src/pages/create-account/create-account.vm.ts
@@ -1,4 +1,19 @@
-export interface NewAccount {
+export interface Account {
   type: string;
   name: string;
 }
+
+export const accountMock: Account[] = [
+  {
+    type: "1",
+    name: "Cuenta Corriente",
+  },
+  {
+    type: "2",
+    name: "Cuenta de Ahorro",
+  },
+  {
+    type: "3",
+    name: "Cuenta de Nómina",
+  },
+];
